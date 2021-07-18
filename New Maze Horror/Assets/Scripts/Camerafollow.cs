54d6a998f837bb3a3cb664dc350a145ae269ac41
bugbakerga <wbrenden82@gmail.com>
2021-07-17 20:18:29 -0500

    added mini map system

---
+++ New Maze Horror/Assets/Scripts/Camerafollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    public float smoothspeed = 0.125f;
+
+
+    void LateUpdate ()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothspeed);
+        transform.position = smoothedPosition;
+    }
+
+}
