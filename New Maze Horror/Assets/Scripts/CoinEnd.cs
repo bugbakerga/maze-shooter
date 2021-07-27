a2e257ed7d7791f3b5ebcf15060d54e978f1c4f7
bugbakerga <wbrenden82@gmail.com>
2021-07-27 11:49:42 -0500

    added enemies to map
    and simple intro animation

---
+++ New Maze Horror/Assets/Scripts/CoinEnd.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinEnd : MonoBehaviour
+{
+    public CoinSystem coinSystem;
+    public int finishNumber;
+
+    public GameObject exitText;
+
+    void Update()
+    {
+        if (coinSystem.coinscollected == finishNumber)
+        {
+            exitText.SetActive(true);
+        }
+    }
+}
---
+++ New Maze Horror/Assets/Scripts/simplelevelintro.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simplelevelintro : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+
+    public bool playonawake;
+    public float transitiondur;
+
+    void Awake()
+    {
+        if (playonawake == true)
+        {
+            Invoke("camTransition", 0.1f);
+        }
+    }
+
+    public void camTransition()
+    {
+        cam2.SetActive(true);
+        cam1.SetActive(false);
+        StartCoroutine(camSequence());
+    }
+
+    IEnumerator camSequence()
+    {
+        yield return new WaitForSeconds(transitiondur);
+        cam1.SetActive(true);
+        cam2.SetActive(false);
+    }
+
+
+}
