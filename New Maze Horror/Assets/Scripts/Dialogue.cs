8166d1519729704cc2163120f083876808477184
bugbakerga <wbrenden82@gmail.com>
2021-08-06 22:46:57 -0500

    added working dialogue system
    includes sequenced camera switching

---
+++ New Maze Horror/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public Text dialoguetext;
+
+    [TextArea(3, 10)]
+    public string[] sentences;
+
+    public int index;
+    public float typingSpeed;
+
+    public GameObject continuebutton;
+    public Animator boxanim;
+
+    public GameObject fpscam;
+    public GameObject dialoguelogic;
+
+    public GameObject movingcam;
+    public Transform[] campos;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+        continuebutton.SetActive(false);
+        boxanim.SetBool("isOpen", true);
+    }
+    
+    void Update()
+    {
+        if(dialoguetext.text == sentences[index])
+        {
+            continuebutton.SetActive(true);
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray()){
+            dialoguetext.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        continuebutton.SetActive(false);
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            dialoguetext.text = "";
+            StartCoroutine(Type());
+            movingcam.transform.position = campos[index].position;
+            movingcam.transform.rotation = campos[index].rotation;
+        }
+        else
+        {
+            continuebutton.SetActive(false);
+            boxanim.SetBool("isOpen", false);
+            fpscam.SetActive(true);
+            dialoguelogic.SetActive(false);
+        }
+    }
+}
---
+++ New Maze Horror/Assets/Scripts/dialoguetrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialoguetrigger : MonoBehaviour
+{
+    public GameObject fpscam;
+    public GameObject dialoguesystem;
+
+    void OnTriggerEnter(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            dialoguesystem.SetActive(true);
+            fpscam.SetActive(false);
+        }
+    }
+}
