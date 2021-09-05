6636903fd9f292dd0a87e6cf3f2c73e83a4ee991
bugbakerga <wbrenden82@gmail.com>
2021-09-05 01:37:17 -0500

    added sum stuff

---
+++ New Maze Horror/Assets/Scripts/bigcanon.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bigcanon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    public bool readtoShoot;
+
+    public float bulletSpeed;
+    public float readySpeed;
+    public int destroySpeed;
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        readtoShoot = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (readtoShoot == true)
+        {
+            StartCoroutine(Shooting());
+        }
+    }
+
+    IEnumerator Shooting()
+    {
+        readtoShoot = false;
+        yield return new WaitForSeconds(readySpeed);
+        transform.LookAt(player);
+        Fire();
+        readtoShoot = true;
+    }
+
+    void Fire()
+    {
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+
+        Destroy(bullet, destroySpeed);
+    }
+}
---
+++ New Maze Horror/Assets/Scripts/noteread.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class noteread : MonoBehaviour
+{
+    private bool readytoread;
+
+    public GameObject readprompt;
+    public GameObject note;
+
+    public AudioSource audioSource;
+    public AudioClip[] clips;
+
+    void OnTriggerEnter(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+        {
+            readytoread = true;
+            readprompt.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+        {
+            readytoread = false;
+            readprompt.SetActive(false);
+            note.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public void buttonend()
+    {
+        note.SetActive(false);
+        readprompt.SetActive(true);
+        audioSource.PlayOneShot(clips[1]);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(readytoread == true)
+        {
+            if (Input.GetButtonDown("Ekey"))
+            {
+                note.SetActive(true);
+                audioSource.PlayOneShot(clips[0]);
+                readprompt.SetActive(false);
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
+}
