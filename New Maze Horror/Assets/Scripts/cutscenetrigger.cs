6529f89b3c3360f6e8a6ff384ee27e31759f07a3
bugbakerga <wbrenden82@gmail.com>
2021-09-19 23:43:57 -0500

    added capture cutscene

---
+++ New Maze Horror/Assets/Scripts/cutscenetrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class cutscenetrigger : MonoBehaviour
+{
+    public PlayableDirector cutscene;
+    public GameObject thePlayer;
+    public GameObject lucky;
+
+    void OnTriggerEnter(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+        {
+            cutscene.Play();
+            thePlayer.SetActive(false);
+            lucky.SetActive(false);
+        }
+    }
+}
