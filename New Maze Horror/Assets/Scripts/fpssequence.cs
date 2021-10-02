b92d0e4c81d8b918eaeac60881cc599b435f331b
bugbakerga <wbrenden82@gmail.com>
2021-10-02 11:13:58 -0500

    added third room and rain

---
+++ New Maze Horror/Assets/Scripts/fpssequence.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class fpssequence : MonoBehaviour
+{
+    public PlayableDirector cutscene;
+
+    void OnTriggerEnter(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            cutscene.Play();
+        }
+    }
+}
