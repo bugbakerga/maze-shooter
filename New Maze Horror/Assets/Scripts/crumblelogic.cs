ba5c52d3b7612ee9e3eaae937760fe36d19edacf
bugbakerga <wbrenden82@gmail.com>
2021-10-16 01:07:11 -0500

    added tracks and transition

---
+++ New Maze Horror/Assets/Scripts/crumblelogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crumblelogic : MonoBehaviour
+{
+    public GameObject crumblefx;
+
+    void OnTriggerEnter(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            Instantiate(crumblefx, transform.position, Quaternion.identity);
+        }
+    }
+}
